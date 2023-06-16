b8a69c549a285d3c496b41bca64959661d00a6be
etboard/ketri <74280054+ketri2484@users.noreply.github.com>
2023-06-16 15:01:24 +0900

    Initial commit

---
+++ src/ETboard/MicroPython/01._uart_led_switch.py
@@ -0,0 +1,80 @@
+# ******************************************************************************************
+# FileName     : 01._uart_led_switch.py
+# Description  : 시리얼 통신으로 '3', '4' 문자를 받으면 파랑 또는 녹색 LED를 깜밖임
+# Author       : 손철수
+# Created Date : 2023.06.16
+# Reference    : 주의1. pyserial 모듈을 import 해야 함
+# Reference    : 주의2. 코딩한 후에 시리얼 통신을 위하여 Thonny가 시리얼 통신 포트를 disconnect
+# Reference    : 주의3. ETboard의 reset 버튼을 눌러야 Thonny가 ETboard를 connect 함
+# Modified     : 
+# ******************************************************************************************
+
+# import
+import time
+from machine import ADC, Pin
+from ETboard.lib.pin_define import *
+from machine import UART
+
+# global variable
+led_red = Pin(D2)                     # 빨강 LED 핀 지정 / heart beat
+led_blue = Pin(D3)                    # 파랑 LED 핀 지정 
+led_green = Pin(D4)                   # 초록 LED 핀 지정
+led_yellow = Pin(D5)                  # 노랑 LED 핀 지정 / 메시지 수신 타임아웃
+uart = UART(1)                        # 시리얼 통신 포트 지정 
+
+# setup
+def setup():    
+    led_red.init(Pin.OUT)             # 빨강 LED를 출력모드 설정
+    led_blue.init(Pin.OUT)            # 파강 LED를 출력모드 설정
+    led_green.init(Pin.OUT)           # 초록 LED를 출력모드 설정
+    led_yellow.init(Pin.OUT)          # 노랑 LED를 출력모드 설정
+    uart.init(baudrate=115200,        # 시리얼 통신 속도 지정
+              timeout=1000,           # 최대 1초만 대기
+              tx=1, rx=3)             # 이티보드 통신 핀 번호 지정
+
+
+# main loop
+def loop():
+    led_red.value(HIGH)               # 빨강 LED 켜기
+    time.sleep(0.5)                   # 0.5초 기다리기
+    
+    receive()    
+
+    led_red.value(LOW)                # 빨강 LED 끄기
+    time.sleep(0.5)                   # 0.5초 기다리기
+
+# receive
+def receive():    
+    msg = uart.readline()             # read a line
+    if msg is None:                   # 아무것도 받지 못하면
+        led_yellow.value(HIGH)        # 노랑 LED 깜밖임
+        time.sleep(0.5)      
+        led_yellow.value(LOW)      
+        time.sleep(0.5)      
+        return
+    
+    cmd = msg.rstrip()                # 받은 메시지 필터링
+    cmd = int(cmd)                    # 문자형을 숫자로 변경
+    
+    if (cmd == 3):                    # 3이면 파랑 LED 깜밖임
+      led_blue.value(HIGH)
+      time.sleep(1)      
+      led_blue.value(LOW)      
+      time.sleep(1)
+    elif (cmd == 4):                   # 4면 녹색 LED 깜밖
+      led_green.value(HIGH)
+      time.sleep(1)      
+      led_green.value(LOW)      
+      time.sleep(1)
+
+if __name__ == "__main__":
+    setup()
+    while True:
+        loop()
+        
+        
+# ==========================================================================================
+#
+#  (주)한국공학기술연구원 http://et.ketri.re.kr
+#
+# ==========================================================================================  
---
+++ src/PC/Python/ETboard_Simple_Remote_Controller_by_Serial.py
@@ -0,0 +1,58 @@
+# ******************************************************************************************
+# FileName     : ETboard_Simple_Remote_Controller_by_Serial
+# Description  : 이티보드를 시리얼 통신으로 원격에서 제어하기
+# Author       :
+# Created Date : 2023.06.16
+# Reference    :
+# Modified     :
+# ******************************************************************************************
+
+
+# import
+import time                             # 시간 모듈 가져오기
+import serial                           # 시리얼 통신 모듈
+
+# global variable
+com_port = 'COM6'                       # 시리얼 포트 번호
+ser = serial.Serial()                   # 시리얼 포트
+
+
+# setup
+def setup():
+    global ser, com_port
+    ser.baudrate = 115200               # 이티보드 시리얼 통신 속도 115,200
+    ser.port = com_port                 # 시리얼 통신 포트 지정
+    ser.open()                          # 시리얼 통신 포트 열기
+    
+
+# main
+def loop():
+    send_etboard()                      # 이티보드에 메시지 보내기
+    time.sleep(1)                       # 1초 기다리기
+
+
+# send_etboard
+def send_etboard():
+    msg = "3"
+    result = bytes(msg + '\n', 'utf-8')
+    ser.write(result)
+    print('send ' + msg + ' to blink blue led')
+    time.sleep(3)
+
+    msg = "4"
+    result = bytes(msg + '\n', 'utf-8')
+    ser.write(result)
+    print('send ' + msg + ' to blink green led')
+    time.sleep(3)
+
+
+if __name__ == "__main__":
+    setup()
+    while True:
+        loop()
+
+# ==========================================================================================
+#
+#  (주)한국공학기술연구원 http://et.ketri.re.kr
+#
+# ==========================================================================================
